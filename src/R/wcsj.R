b4f2e55320b2dd5de68191a5f5950b414fd5002f
Syed Hussain Ather <shussainather@gmail.com>
2019-12-03 01:03:43 -0800

    Wcsj

--- src/R/wcsj.R
+++ src/R/wcsj.R
@@ -296,3 +296,22 @@ ggplot(immun_counties_year, aes(x = start_year, y = county)) +
         legend.position="bottom",
         legend.key.height = unit(0.4, "cm")) +
   ggtitle("Immunization in California kindergartens, by county")
+
+# Load data.
+nations <- read_csv("data/wcsj/nations.csv")
+
+# Filter for 2015 data only.
+nations2015 <- nations %>%
+  filter(year == 2015)
+
+# Make bubble chart.
+ggplot(nations2015, aes(x = gdp_percap, y = life_expect)) +
+  xlab("GDP per capita") +
+  ylab("Life expectancy at birth") +
+  theme_minimal(base_size = 12, base_family = "Georgia") +
+  geom_point(aes(size = population, color = region), alpha = 0.7) +
+  scale_size_area(guide = FALSE, max_size = 15) +
+  scale_x_continuous(labels = dollar) +
+  stat_smooth(formula = y ~ log10(x), se = FALSE, size = 0.5, color = "black", linetype="dotted") +
+  scale_color_brewer(name = "", palette = "Set2") +
+  theme(legend.position=c(0.8,0.4))
