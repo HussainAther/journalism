e0cd19e45a8493a88b657bbf36e6161738bfe52e
Syed Hussain Ather <shussainather@gmail.com>
2019-12-03 00:38:12 -0800

    install and load

--- src/R/wcsj.R
+++ src/R/wcsj.R
@@ -1,2 +1,6 @@
 setwd("../..") # Move up to the journalism directory
 save.image("data/wcsj/wcsj.RData") # Save data here 
+install.packages("tidyverse") # Install tidyverse, if you haven't already
+library(readr) # Load required packages
+library(dplyr)
+library(ggplot2)
