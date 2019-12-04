0b415b17d97bdec429b099788e4995bb43924840
Syed Hussain Ather <shussainather@gmail.com>
2019-12-04 11:18:40 -0800

    data

--- src/R/wbi.R
+++ src/R/wbi.R
@@ -1,1 +1,6 @@
+library(readr)
+library(dplyr)
 "World Bank Indicators"
+setwd("../..") # Move up to the journalism directory
+nations1 <- read_csv("data/wbi/nations1.csv")
+nations2 <- read_csv("data/wbi/nations2.csv")
