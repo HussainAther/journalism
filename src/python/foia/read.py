e9e0a889a399e9cfeb322d959ebfabe210bfa2de
Syed Hussain Ather <shussainather@gmail.com>
2019-12-06 16:25:40 -0800

    just pandas

--- src/python/foia/read.py
+++ src/python/foia/read.py
@@ -1,9 +1,8 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 """
-Read and analyze the FOIA data.
+Read the FOIA data.
 """
 
 df = pd.read_csv("data/foia/foiacontacts.csv")
+print(df.head)
