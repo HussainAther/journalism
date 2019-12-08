e81ddfd5652f1d04b9d229a95eefe79f74d5f1a1
Syed Hussain Ather <shussainather@gmail.com>
2019-12-08 01:37:17 -0800

    read check

--- src/python/geocode/jsonread.py
+++ src/python/geocode/jsonread.py
@@ -1,9 +1,10 @@
+import geopandas as gpd
 import json
 
 """
 Read in the geocoding file.
 """
 
-with open("data/src/geocode/refine_geocoder.json") as f:
+with open("data/geocode/refine_geocoder.json") as f:
     data = json.load(f)
 
