8424f8968f33eaf8d62e1cc758d942a592e1cca2
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 21:18:30 -0800

    output freq

--- src/python/dam/read.py
+++ src/python/dam/read.py
@@ -6,5 +6,12 @@ Read in and analyze the Oregon dam data.
 """
 
 df = pd.read_csv("data/dam/oregondams.csv")
+cfreqs = df["county"].value_counts() # frequency of each county
+cindex = df["county"].value_counts().index # county name
 
-plt.bar(df["county"].drop_duplicates(), df["county"].value_counts())
+plt.bar(cindex, cfreqs)
+plt.xticks(range(len(cindex)), cindex, rotation="vertical")
+plt.xlabel("County")
+plt.ylabel("Number of dams")
+plt.title("Dam frequency by county")
+plt.savefig("output/dam/freq.png")
