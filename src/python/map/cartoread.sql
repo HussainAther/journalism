097f2e4e73848e7fe66a181ae4e991160d5f4f86
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 20:40:22 -0800

    cartodb

--- src/python/map/cartoread.sql
+++ src/python/map/cartoread.sql
@@ -1,4 +1,7 @@
 --SQL (Structured query language) for Carto mapping
 --Select all data from the table
 SELECT * FROM ne_50m_admin_1_states
-
+--CartoDB code to create dataset
+SELECT *
+FROM ne_50m_admin_1_states
+WHERE postal = 'OR'
