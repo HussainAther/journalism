218243b0302e72144511cc6b834808238a616dd9
Syed Hussain Ather <shussainather@gmail.com>
2019-12-11 01:11:23 -0800

    animate

--- src/R/animation.R
+++ src/R/animation.R
@@ -27,3 +27,21 @@ ggplot(nations2016, aes(x = gdp_percap, y = life_expect)) +
   stat_smooth(formula = y ~ log10(x), se = FALSE, size = 0.5, color = "black", linetype="dotted") +
   scale_color_brewer(name = "", palette = "Set2") +
   theme(legend.position=c(0.8,0.4))
+
+# Animate entire time series with gganimate.
+nations_plot <- ggplot(nations, aes(x = gdp_percap, y = life_expect)) +
+  xlab("GDP per capita") +
+  ylab("Life expectancy at birth") +
+  theme_minimal(base_size = 14, base_family = "Georgia") +
+  geom_point(aes(size = population, color = region), alpha = 0.7) +
+  scale_size_area(guide = FALSE, max_size = 15) +
+  scale_x_continuous(labels = dollar) +
+  stat_smooth(formula = y ~ log10(x), se = FALSE, size = 0.5, color = "black", linetype="dotted") +
+  scale_color_brewer(name = "", palette = "Set2") +
+  theme(legend.position=c(0.8,0.4)) +
+  # gganimate code
+  ggtitle("{frame_time}") +
+  transition_time(year) +
+  ease_aes("linear") +
+  enter_fade() +
+  exit_fade()
