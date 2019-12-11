e29b009530ff662ef56aa6d4d5b2f121ceb99504
Syed Hussain Ather <shussainather@gmail.com>
2019-12-11 01:10:24 -0800

    filter

--- src/R/animation.R
+++ src/R/animation.R
@@ -1,9 +1,17 @@
-# load required packages
+# Load required packages.
 library(readr)
 library(ggplot2)
 library(gganimate)
 library(scales)
 library(dplyr)
 
-# load data
+# Load data.
 nations <- read_csv("data/wcsj/nations.csv")
+
+# Make sure that year is treated as an integer.
+nations <- nations %>%
+    mutate(year = as.integer(year))
+
+# Filter for 2016 data only.
+nations2016 <- nations %>%
+  filter(year == 2016)
