549586758251e2f936228b24ae0c474f6172b078
Syed Hussain Ather <shussainather@gmail.com>
2019-12-14 20:23:37 -0800

    fixed dups

--- src/python/criticalminerals/periodic.py
+++ src/python/criticalminerals/periodic.py
@@ -3,7 +3,7 @@ from bokeh.plotting import figure
 from bokeh.sampledata.periodic_table import elements
 from bokeh.transform import dodge, factor_cmap
 
-output_file("periodic.html")
+output_file("../../../output/criticalminerals/critmincolor.html")
 
 periods = ["I", "II", "III", "IV", "V", "VI", "VII"]
 groups = [str(x) for x in range(1, 19)]
@@ -16,86 +16,58 @@ df = df[df.group != "-"]
 df = df[df.symbol != "Lr"]
 df = df[df.symbol != "Lu"]
 
-cmap = {
-    "alkali metal"         : "#a6cee3",
-    "alkaline earth metal" : "#1f78b4",
-    "metal"                : "#d93b43",
-    "halogen"              : "#999d9a",
-    "metalloid"            : "#e08d49",
-    "noble gas"            : "#eaeaea",
-    "nonmetal"             : "#f1d4Af",
-    "transition metal"     : "#599d7A",
-}
-
-TOOLTIPS = [
-    ("Name", "@name"),
-    ("Atomic number", "@{atomic number}"),
-    ("Atomic mass", "@{atomic mass}"),
-    ("Type", "@metal"),
-    ("CPK color", "$color[hex, swatch]:CPK"),
-    ("Electronic configuration", "@{electronic configuration}"),
-]
-
-p = figure(title="Periodic Table (omitting LA and AC Series)", plot_width=1000, plot_height=450,
-           x_range=groups, y_range=list(reversed(periods)),
-           tools="hover", toolbar_location=None, tooltips=TOOLTIPS)
-
-r = p.rect("group", "period", 0.95, 0.95, source=df, fill_alpha=0.6, legend="metal",
-           color=factor_cmap("metal", palette=list(cmap.values()), factors=list(cmap.keys())))
-
-text_props = {"source": df, "text_align": "left", "text_baseline": "middle"}
-
-x = dodge("group", -0.4, range=p.x_range)
-
-p.text(x=x, y="period", text="symbol", text_font_style="bold", **text_props)
-
-p.text(x=x, y=dodge("period", 0.3, range=p.y_range), text="atomic number",
-       text_font_size="8pt", **text_props)
-
-p.text(x=x, y=dodge("period", -0.35, range=p.y_range), text="name",
-       text_font_size="5pt", **text_props)
-
-p.text(x=x, y=dodge("period", -0.2, range=p.y_range), text="atomic mass",
-       text_font_size="5pt", **text_props)
-
-p.text(x=["3", "3"], y=["VI", "VII"], text=["LA", "AC"], text_align="center", text_baseline="middle")
-
-p.outline_line_color = None
-p.grid.grid_line_color = None
-p.axis.axis_line_color = None
-p.axis.major_tick_line_color = None
-p.axis.major_label_standoff = 0
-p.legend.orientation = "horizontal"
-p.legend.location ="top_center"
-p.hover.renderers = [r] # only hover element boxes
-
-show(p)from bokeh.io import output_file, show
-from bokeh.plotting import figure
-from bokeh.sampledata.periodic_table import elements
-from bokeh.transform import dodge, factor_cmap
-
-output_file("periodic.html")
-
-periods = ["I", "II", "III", "IV", "V", "VI", "VII"]
-groups = [str(x) for x in range(1, 19)]
-
-df = elements.copy()
-df["atomic mass"] = df["atomic mass"].astype(str)
-df["group"] = df["group"].astype(str)
-df["period"] = [periods[x-1] for x in df.period]
-df = df[df.group != "-"]
-df = df[df.symbol != "Lr"]
-df = df[df.symbol != "Lu"]
-
-cmap = {
-    "alkali metal"         : "#a6cee3",
-    "alkaline earth metal" : "#1f78b4",
-    "metal"                : "#d93b43",
-    "halogen"              : "#999d9a",
-    "metalloid"            : "#e08d49",
-    "noble gas"            : "#eaeaea",
-    "nonmetal"             : "#f1d4Af",
-    "transition metal"     : "#599d7A",
+# cmap = {
+#     "alkali metal"         : "#a6cee3",
+#     "alkaline earth metal" : "#1f78b4",
+#     "metal"                : "#d93b43",
+#     "halogen"              : "#999d9a",
+#     "metalloid"            : "#e08d49",
+#     "noble gas"            : "#eaeaea",
+#     "nonmetal"             : "#f1d4Af",
+#     "transition metal"     : "#599d7A",
+# }
+cmap : {"Sb":"#D1C7C7",
+"Ba":"#BAABAB",
+"Be":"#AD1F1F",
+"Li":"#A65959",
+"Nb":"#996666",
+"Ta":"#996666",
+"Sn":"#A65959",
+"Ti":"#854747",
+"Ga":"#FF3333",
+"Co":"#BF4040",
+"Ru":"#C27070",
+"Rh":"#C27070",
+"Pt":"#C27070",
+"Pd":"#C27070",
+"Ir":"#C27070",
+"Os":"#C27070",
+"Mn":"#D1C7C7",
+"F":"#B87A7A",
+"Ge":"#E8E3E3",
+"In":"#E8E3E3",
+"Ce":"#AD8585",
+"Dy":"#AD8585",
+"Er":"#AD8585",
+"Eu":"#AD8585",
+"Gd":"#AD8585",
+"Ho":"#AD8585",
+"La":"#AD8585",
+"Lu":"#AD8585",
+"Nd":"#AD8585",
+"Pr":"#AD8585",
+"Pm":"#AD8585",
+"Sm":"#AD8585",
+"Sc":"#AD8585",
+"Tb":"#AD8585",
+"Tm":"#AD8585",
+"Yb":"#AD8585",
+"Y":"#AD8585",
+"Te":"#7A5252",
+"Re":"#B87A7A",
+"Zr":"#E8E3E3",
+"Hf":"#E8E3E3",
+"V":"#C99C9C"
 }
 
 TOOLTIPS = [
