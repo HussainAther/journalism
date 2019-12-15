29c52ec4de2e90c5a9f86b25a2e538180852a2b9
Syed Hussain Ather <shussainather@gmail.com>
2019-12-14 23:44:30 -0800

    begin js

--- src/python/googlemap/geochart.py
+++ src/python/googlemap/geochart.py
@@ -5,3 +5,10 @@ import gviz_api
 Visualize disposable median monthly salary per country on the world map
 projection using Google Geochart and Table Visualization.
 """
+
+def gettemplate():
+    pagetemplate = """
+    <html>
+          <script src="https://www.google.com/jsapi" type="text/javascript"></script>
+     <script>
+      google.load("visualization"", "1", {packages:
