9f4a8abc2117aa8cbd7c75a7a17787852d894b7b
Syed Hussain Ather <shussainather@gmail.com>
2019-12-14 19:06:45 -0800

    import

--- src/python/criticalminerals/correlation.py
+++ src/python/criticalminerals/correlation.py
@@ -1,4 +1,11 @@
+import matplotlib.py plot as plt
+import numpy as np
+import pandas as pd
+
 """
 Search for correlations among the periodic table
 of critical minerals.
 """
+
+
+df = "data/criticalminerals/input.csv"
