30d7b9aef4ec37855c7eab8644372cb9c2e6b1b0
Syed Hussain Ather <shussainather@gmail.com>
2019-12-15 02:19:35 -0800

    no save

--- src/R/wcsj.R
+++ src/R/wcsj.R
@@ -1,5 +1,5 @@
 setwd("../..") # Move up to the journalism directory
-save.image("data/wcsj/wcsj.RData") # Save data here 
+# save.image("data/wcsj/wcsj.RData") # Save data here 
 install.packages("tidyverse") # Install tidyverse, if you haven't already
 library(readr) # Load required packages
 library(dplyr)
