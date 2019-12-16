5f92017c3637a5d4dcb21df778bda3fa1c175dd7
Syed Hussain Ather <shussainather@gmail.com>
2019-12-16 00:35:21 -0800

    error

--- src/python/twitter/tweetread.py
+++ src/python/twitter/tweetread.py
@@ -16,3 +16,32 @@ consumerkey = sys.argv[1]
 consumersecret = sys.argv[2]
 accesstoken = sys.argv[3] 
 accesssecret = sys.arv[4]
+
+
+class TweetsListener(StreamListener):
+
+    def init(self, csocket):
+        """
+        Initialize the client socket. 
+        """
+        self.clientsocket = csocket
+
+    def ondata(self, data):
+        """
+        Collecting message data.
+        """
+        try:
+            msg = json.loads( data )
+            print(msg["text"].encode("utf-8"))
+            self.clientsocket.send( msg["text"].encode("utf-8") )
+            return True
+        except BaseException as e:
+            print("Error ondata: %s" % str(e))
+        return True
+  
+    def onerror(self, status):
+        """
+        If we have an error
+        """
+        print(status)
+        return True
