f0802fa373780bd27bd57706e4e4c69d84e3ab81
Syed Hussain Ather <shussainather@gmail.com>
2019-12-16 00:29:15 -0800

    credentials

--- src/python/twitter/tweetread.py
+++ src/python/twitter/tweetread.py
@@ -1,7 +1,18 @@
 import json
 import socket
+import sys
 import tweepy
 
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
+
+"""
+Read tweets.
+"""
+
+# Set up your credentials.
+consumerkey = sys.argv[1]
+consumersecret = sys.argv[2]
+accesstoken = sys.argv[3] 
+accesssecret = sys.arv[4]
