95b617c7f21ba2ea8956ad3427128ee4780e82aa
Syed Hussain Ather <shussainather@gmail.com>
2019-12-17 01:49:23 -0800

    simplify output

--- src/python/misc/makewordcloud.py
+++ src/python/misc/makewordcloud.py
@@ -1,6 +1,4 @@
 import nltk
-from nltk.corpus import stopwords, wordnet
-from nltk.tokenize import word_tokenize 
 from wordcloud import WordCloud
 en_stop = nltk.download("stopwords")
-WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color="steelblue").generate_from_text(" ".join([r for r in open("mobydick.txt", "r").read().split()] if r not in set(stopwords.words("english"))).to_file("sys.argv[1] + "_wordcloud.png")
+WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color="steelblue").generate_from_text(" ".join([r for r in open("mobydick.txt", "r").read().split()] if r not in set(nltk.corpus.stopwords.words("english"))).to_file("wordcloud.png")
