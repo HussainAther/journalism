55f3a03409aee31bec21d05a6b2c8f273ed9bf64
Syed Hussain Ather <shussainather@gmail.com>
2019-12-18 02:52:11 -0800

    mentions

--- src/python/twitter/interactive.py
+++ src/python/twitter/interactive.py
@@ -21,3 +21,21 @@ df = pd.read_json("tweets.txt")
 # Get the info we want.
 eqcol = ["created_at", "id", "text"]
 final[eqcol] = df[eqcol]
+
+def getbasics(tfinal):
+    """
+    Get the basic information about the user.
+    """
+    tfinal["screen_name"] = tweets_df["user"].apply(lambda x: x["screen_name"])
+    tfinal["user_id"] = tweets_df["user"].apply(lambda x: x["id"])
+    tfinal["followers_count"] = tweets_df["user"].apply(lambda x: x["followers_count"])
+    return tfinal
+
+def getusermentions(tfinal):
+    """
+    Get user mentions.
+    """
+    # Inside the tag "entities" will find "user mentions" and will get "screen name" and "id"
+    tfinal["user_mentions_screen_name"] = tweets_df["entities"].apply(lambda x: x["user_mentions"][0]["screen_name"] if x["user_mentions"] else np.nan)
+    tfinal["user_mentions_id"] = tweets_df["entities"].apply(lambda x: x["user_mentions"][0]["id_str"] if x["user_mentions"] else np.nan)
+    return tfinal
