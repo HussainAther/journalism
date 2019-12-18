8eebc26e17957b3f891d7a55b1fbbc66f8a428d7
Syed Hussain Ather <shussainather@gmail.com>
2019-12-18 02:50:11 -0800

    get it

--- src/python/twitter/interactive.py
+++ src/python/twitter/interactive.py
@@ -2,6 +2,7 @@ import json
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import pandas as pd
 import plotly.graph_objects as go
 import re
 
@@ -13,3 +14,10 @@ followers are from one another.
 """
 
 pd.set_option("display.float_format", lambda x: "%.f" % x)
+
+# Read JSON into a pandas DataFrame.
+df = pd.read_json("tweets.txt")
+
+# Get the info we want.
+eqcol = ["created_at", "id", "text"]
+final[eqcol] = df[eqcol]
