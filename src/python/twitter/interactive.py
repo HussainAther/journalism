5cfbfc10ea13500b8b982feb4f2ccce8324e0cfa
Syed Hussain Ather <shussainather@gmail.com>
2019-12-18 02:02:44 -0800

    import

--- src/python/twitter/interactive.py
+++ src/python/twitter/interactive.py
@@ -1,6 +1,14 @@
+import json
+import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 import plotly.graph_objects as go
+import re
+
+from operator import itemgetter
 
 """
-Create an interactive network
+Create an interactive network of how distant Twitter
+followers are from one another.
 """
+
