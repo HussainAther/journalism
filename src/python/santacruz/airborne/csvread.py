4eeaec9dcc7081ca9141f0f4897132eacee45323
Syed Hussain Ather <shussainather@gmail.com>
2019-12-19 14:26:34 -0800

    still working on legend

--- src/python/santacruz/airborne/csvread.py
+++ src/python/santacruz/airborne/csvread.py
@@ -5,8 +5,6 @@ import pandas as pd
 import plotly.graph_objs as go
 import seaborn as sns
 
-from clustergrammer import Network
-
 """
 Visualize the quadrangle of magnetic activity for the Santa Cruz area.
 """
@@ -23,38 +21,37 @@ plot = sns.lmplot("latitude", "longitude", data=magdf, fit_reg=False, hue="diurn
 plot.savefig("output/santacruz/airborne/quadrangle.png")
 
 """
-Make an interactive map with clustergrammar.
-
-# Initialize.
-net = Network()
-
-# Load the file.
-net.load_file("data/santacruz/airborne/magsimple.tsv")
-
-net.cluster(enrichrgram=False)
-
-net.widget()
-"""
-
-"""
 Make an interactive map with plotly.
 """
 
 # Load the file.
-simplemagdf = pd.read_csv("data/santacruz/airborne/magsimplecut.csv")
+simplemagdf = pd.read_csv("data/santacruz/airborne/magsimple.csv")
 # magdfmatrix = simplemagdf.pivot("latitude", "longitude", "diurnal")
 
 # Get the magnetic field as a float.
-mf = simplemagdf["diurnal"].astype(float)
+simplemagdf["diurnal"] = simplemagdf["diurnal"].astype(float)
 
-# Set the color scale.
-colorscale = [[50375, "#edf8fb"], [50383.5, "#b3cde3"],  [50391.5, "#8856a7"],  [50399, "#810f7c"]]
+# Cut the data for plotly size constraints.
+simplemagdf = simplemagdf[(simplemagdf["latitude"] >= 36.4) & (simplemagdf["latitude"] <= 36.6)]
+simplemagdf = simplemagdf[(simplemagdf["longitude"] >= -121.5) & (simplemagdf["longitude"] <= -120.5)]
 
-# Plot.
-heatmap = go.Heatmap(z=simplemagdf["diurnal"], 
-                     x=simplemagdf["latitude"], 
-                     y=mf)
-#                     colorscale=colorscale)
-data = [heatmap]
-py.iplot(data, filename="airborneheatmap.html")
+# Create figure.
+fig = go.Figure()
 
+fig.add_trace(
+    go.Scattergl(
+        x = simplemagdf["latitude"],
+        y = simplemagdf["longitude"],
+        mode = "markers",
+        marker = dict(
+                color=simplemagdf["diurnal"],
+                colorscale="Viridis"),
+        hovertext=[simplemagdf["latitude"], 
+                    simplemagdf["longitude"],
+                    simplemagdf["diurnal"]],
+        showlegend=True
+    )
+)
+
+fig.update_layout(title="Magnetic field (nT) across Santa Cruz")
+py.plot(fig)
