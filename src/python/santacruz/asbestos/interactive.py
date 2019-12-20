5ecbd234e0c07e28b6a223fde9013805e25a20e4
Syed Hussain Ather <shussainather@gmail.com>
2019-12-19 21:35:31 -0800

    mapbox access token

--- src/python/santacruz/asbestos/interactive.py
+++ src/python/santacruz/asbestos/interactive.py
@@ -9,6 +9,8 @@ from shapely.geometry import Point
 Let's get down to business to defeat asbestos.
 """
 
+px.set_mapbox_access_token(open(".mapbox_token").read())
+
 # Read data.
 df = pd.read_csv("data/santacruz/asbestos/main.csv")
 
@@ -17,7 +19,12 @@ geom = [Point(xy) for xy in zip(df["longitude"], df["latitude"])]
 gdf = gpd.GeoDataFrame(
     df, geometry=geom)
 
+# Restrict to America.
+country = gpd.read_file("data/gz_2010_us_040_00_5m.json")
+country = country[country["NAME"].isin(["Alaska","Hawaii"]) == False]
+
 # Plot.
-fig = px.scatter_mapbox(gdf, lat="latitude", lon="longitude", hover_data=["county", "state"]) 
+fig, ax = plt.subplots()
+px.scatter_mapbox(gdf, lat="latitude", lon="longitude", hover_data=["county", "state"]) 
 fig.show()
 
