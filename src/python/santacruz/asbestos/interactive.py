d4f12e48d76fa130c7de3e60a9ea1481828c0626
Syed Hussain Ather <shussainather@gmail.com>
2019-12-19 16:44:16 -0800

    init

---
+++ src/python/santacruz/asbestos/interactive.py
@@ -0,0 +1,17 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from shapely.geometry import Point
+
+"""
+Let's get down to business to defeat asbestos.
+"""
+
+# Read data.
+df = pd.read_csv("data/asbestos/main.csv")
+
+# Extract data.
+geom = [Point(xy) for xy in zip(df["longitude"], df["latitude"])]
+gdf = gpd.GeoDataFrame(
+    df, geometry=geom)
