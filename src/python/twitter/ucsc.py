1a97abe6432ed1d6decdc623863c00566c8d8dcf
Syed Hussain Ather <shussainather@gmail.com>
2019-12-20 20:44:37 -0800

    screen name and simplified

--- src/python/twitter/ucsc.py
+++ src/python/twitter/ucsc.py
@@ -23,16 +23,10 @@ screennames = ["science_ari", "shussainather", "laragstreiff",
                 "erinmalsbury", "joetting13", "jonathanwosen",
                 "heysmartash"]
 
-CONSUMER_KEY = os.getenv("CONSUMER_KEY")
-CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
-ACCESS_TOKEN_KEY = os.getenv("ACCESS_TOKEN_KEY")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
-
-def getfilenames(sn):
-    """
-    Build the friends, followers and tweets filenames.
-    """
-    return os.path.join(datadir, "%s.friends.pickle" % (sn)), os.path.join(datadir, "%s.followers.pickle" % (sn))
+CONSUMER_KEY = "OKCz6x0AWToiTM8ZmQ2uqYH5u"
+CONSUMER_SECRET = "fTbIoojwok3oymzFGZQ0uv7OoXZFyA29GQTEbpi2XjH3oxG662"
+ACCESS_TOKEN_KEY = "1149121881217359872-EnxJZ67rtoxs3iXqJeasFGduCa6WDz"
+ACCESS_TOKEN_SECRET = "koXMF3a1Uy15ZBawtWiLE7WTluW5pd24uNfhuHmy8pWvO"
 
 if __name__ == "__main__":
     """
@@ -42,23 +36,15 @@ if __name__ == "__main__":
                     consumer_secret = CONSUMER_SECRET,
                     access_token_key = ACCESS_TOKEN_KEY,
                     access_token_secret = ACCESS_TOKEN_SECRET)
-    for sn in screennames:
+    for sn in ["shussainather"]:
         """
-        For each user, get the friends, followers and tweets and save them
+        For each user, get the followers and tweets and save them
         to output pickle and JSON files.
         """
-        fr, fo = getfilenames(sn)
-        # Get the friends infromation.
-	frf = t.GetFriends(user_id = sn)
-        with open(fr, "w") as frfpickle: # Open a new file for the pickle.
-            pickle.dump(frf, frfpickle, protocol = 2) # Write out to a pickle file.
-        with open(fr, "r") as frfpickle: # Open the pickle file.
-            with open(fr.replace(".pickle", ".json"), "w") as frfjson: 
-                frfdata = pickle.load(frfpickle) # Get the pickle file's contents.
-                json.dump(frfdata, frfjson) # Write to a JSON file.
-   
         # Get the follower information.
-        fof = t.GetFollowers(user_id = sn)
+        fof = t.GetFollowers(screen_name = sn)
+        print(fof)
+        fo = datadir + "/" + sn + ".followers.pickle" 
         with open(fo, "w") as fofpickle:
             pickle.dump(fof, fofpickle, protocol = 2)
         with open(fo, "r") as fofpickle:
@@ -66,8 +52,8 @@ if __name__ == "__main__":
                 fofdata = pickle.load(fofpickle)
                 json.dump(fofdata, fofjson)      
 
-        # Get the user's timeline with the ten most recent tweets.
-        timeline = t.GetUserTimeline(screen_name=sn, count=500)
-        tweets = [i.AsDict() for i in timeline]
-        with open(datadir + "/" + sn + ".tweets.json", "w") as tweetsjson:
-            json.dump(tweets, tweetsjson) # Store the informtion in a JSON. 
+        # # Get the user's timeline with the ten most recent tweets.
+        # timeline = t.GetUserTimeline(screen_name=sn, count=500)
+        # tweets = [i.AsDict() for i in timeline]
+        # with open(datadir + "/" + sn + ".tweets.json", "w") as tweetsjson:
+        #     json.dump(tweets, tweetsjson) # Store the informtion in a JSON. 
