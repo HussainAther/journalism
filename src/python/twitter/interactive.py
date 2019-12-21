0e5c1e29fa21229becfca82c094e1e42fa9d7a53
Syed Hussain Ather <shussainather@gmail.com>
2019-12-20 20:55:52 -0800

    indent and input

--- src/python/twitter/interactive.py
+++ src/python/twitter/interactive.py
@@ -6,16 +6,18 @@ import pandas as pd
 import sys
 import urllib2
 
+from plotly.offline import iplot, plot
+
 """
 Create an interactive network of how distant Twitter
 followers are from one another.
 """
 
 # Read input JSON file.
-data = sys.argv[1]
+inputjson = sys.argv[1]
 
 # Read JSON into a pandas DataFrame.
-df = pd.read_json(tweetsjson)
+df = pd.read_json(inputjson)
 
 # Initialize the network graph.
 graph = nx.Graph()
@@ -30,3 +32,52 @@ for index, tweet in tfinal.iterrows():
         graph.node[user_id]["name"] = user_name
         graph.node[int_id]["name"] = int_name
 
+# Plot.
+# Get the edges.
+Xe=[]
+Ye=[]
+for e in G.edges():
+    Xe.extend([pos[e[0]][0], pos[e[1]][0], None])
+    Ye.extend([pos[e[0]][1], pos[e[1]][1], None])
+
+# Trace.
+trace_nodes=dict(type="scatter",
+                 x=Xn, 
+                 y=Yn,
+                 mode="markers",
+                 marker=dict(size=28, color="rgb(0,240,0)"),
+                 text=labels,
+                 hoverinfo="text")
+trace_edges=dict(type="scatter",                  
+                 mode="lines",                  
+                 x=Xe,                  
+                 y=Ye,                 
+                 line=dict(width=1, color="rgb(25,25,25)"), hoverinfo="none")
+
+# Use the Fruchterman Reingold (Fruchterman-Reingold) layout algorithm to plot.
+axis=dict(showline=False, 
+          zeroline=False,
+          showgrid=False,
+          showticklabels=False,
+          title=""
+          )
+layout=dict(title= "My Grapha",  
+             font= dict(family="Balto"),
+             width=600,
+             height=600,
+             autosize=False,
+             showlegend=False,
+             xaxis=axis,
+             yaxis=axis,
+             margin=dict(
+             l=40,
+             r=40,
+             b=85,
+             t=100,
+             pad=0,   
+     ),
+     hovermode="closest",
+     plot_bgcolor="#EFECEA", #set background color            
+     )
+fig = dict(data=[trace_edges, trace_nodes], layout=layout)
+iplot(fig)
