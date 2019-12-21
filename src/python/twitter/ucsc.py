241cf565b01f5c4cd6a5a61b80103bb85bb3d94e
Syed Hussain Ather <shussainather@gmail.com>
2019-12-20 20:51:51 -0800

    rm private info

--- src/python/twitter/ucsc.py
+++ src/python/twitter/ucsc.py
@@ -23,10 +23,10 @@ screennames = ["science_ari", "shussainather", "laragstreiff",
                 "erinmalsbury", "joetting13", "jonathanwosen",
                 "heysmartash"]
 
-CONSUMER_KEY = "OKCz6x0AWToiTM8ZmQ2uqYH5u"
-CONSUMER_SECRET = "fTbIoojwok3oymzFGZQ0uv7OoXZFyA29GQTEbpi2XjH3oxG662"
-ACCESS_TOKEN_KEY = "1149121881217359872-EnxJZ67rtoxs3iXqJeasFGduCa6WDz"
-ACCESS_TOKEN_SECRET = "koXMF3a1Uy15ZBawtWiLE7WTluW5pd24uNfhuHmy8pWvO"
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN_KEY = os.getenv("ACCESS_TOKEN_KEY")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 if __name__ == "__main__":
     """
