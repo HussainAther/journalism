2ce680514258946ecc09db88f5d5f4acde2447cd
Syed Hussain Ather <shussainather@gmail.com>
2021-01-19 21:58:56 -0500

    updated using new one-liner

--- src/python/misc/makewordcloud.py
+++ src/python/misc/makewordcloud.py
@@ -1,4 +1,5 @@
 import nltk
 from wordcloud import WordCloud
 nltk.download("stopwords")
-WordCloud().generate_from_text(" ".join([r for r in open("mobydick.txt", "r").read().split()] if r not in set(nltk.corpus.stopwords.words("english"))).to_file("wordcloud.png")
+
+WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color="steelblue").generate_from_text(" ".join([r for r in open("mobydick.txt", "r").read().split() if r not in set(nltk.corpus.stopwords.words("english"))])).to_file("wordcloud.png")
